5ef5f0405e7b02a0074dde3ccd8295802699a8ec
Vinicius Nobre <vina.santos06@gmail.com>
2025-05-21 16:35:48 -0300

    Add files via upload

--- src/routes.ts
+++ src/routes.ts
@@ -64,6 +64,9 @@ routes.get("/ex", (req: Request, res: Response) : any =>{
 })
 
 routes.post("/users", (req: Request, res: Response) : any => userController.create(req, res))
-
+routes.get("/users", (req: Request, res: Response) : any => userController.read(req, res))
+routes.put("/users/:id", (req: Request, res: Response) : any => userController.update(req, res))
+routes.delete("/users/:id", (req: Request, res: Response) : any => userController.delete(req, res))
+routes.post("/login", (req: Request, res: Response) : any => userController.login(req, res))
 
 export default routes
